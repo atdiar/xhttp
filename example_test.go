e88f803538aca667aae7087f75f5ccb59058bbd9
Taye <atdiar@gmail.com>
2016-06-03 23:10:55 +0200

    xhttp: Improve tests especially for Handler Chaining
    Handler chaining was not correct due to a design flaw in the implementation.
    The mistake was not caught by the tests which were not extensive enough in terms of functionality coverage.
    This commit is making up for this.

--- example_test.go
+++ example_test.go
@@ -10,18 +10,35 @@ import (
 	"github.com/atdiar/xhttp"
 )
 
+const (
+	A = "A "
+	B = "B "
+	C = "C "
+)
+
 func Example() {
 	s := xhttp.NewServeMux()
-
-	s.USE(middlewareExample{})
+	m := xhttp.Chain(middlewareExample{A, nil}, middlewareExample{B, nil}, middlewareExample{C, nil})
+	s.USE(m)
 
 	s.GET("/", xhttp.HandlerFunc(func(ctx execution.Context, res http.ResponseWriter, req *http.Request) {
-		ctx.Put("test", "OK")
-		val, err := ctx.Get("test")
+		a, err := ctx.Get(A)
 		if err != nil {
-			panic(err) // for the sake of the example, we will panic. Not idiomatic.
+			fmt.Fprint(res, err) // shall make the test fail
 		}
-		fmt.Fprint(res, val)
+		fmt.Fprint(res, a)
+
+		b, err := ctx.Get(B)
+		if err != nil {
+			fmt.Fprint(res, err) // shall make the test fail
+		}
+		fmt.Fprint(res, b)
+
+		c, err := ctx.Get(C)
+		if err != nil {
+			fmt.Fprint(res, err) // shall make the test fail
+		}
+		fmt.Fprint(res, c)
 	}))
 
 	req, err := http.NewRequest("GET", "http://example.com/", nil)
@@ -33,18 +50,20 @@ func Example() {
 	s.ServeHTTP(w, req)
 
 	fmt.Printf("%d - %s", w.Code, w.Body.String())
-	// Output: 200 - OK OK
+	// Output: 200 - OK OK OK A B C
 }
 
 // Let's create a catchAll Handler i.e. an object implementing HandlerLinker.
 // It's also what people may call a catchall middleware.
 // This should illustrate one of the form a HandlerLinker can take.
 type middlewareExample struct {
+	string
 	next xhttp.Handler
 }
 
 func (m middlewareExample) ServeHTTP(ctx execution.Context, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "OK ")
+	ctx.Put(m.string, m.string)
 	if m.next != nil {
 		m.next.ServeHTTP(ctx, w, r)
 	}
