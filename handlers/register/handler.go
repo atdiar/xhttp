432236b0db23b2f9f86ca9efd16c6c7e5306bfcf
atdiar <atdiar@gmail.com>
2017-06-01 22:31:23 +0200

    xhttp:register stub unfinished

--- handlers/register/handler.go
+++ handlers/register/handler.go
@@ -1,7 +1,6 @@
 package register
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/atdiar/errors"
@@ -9,38 +8,47 @@ import (
 	"github.com/atdiar/goroutine/execution"
 )
 
-// Register is a structured type based of the data kept for user registration.
+// User is a structured type based of the data kept for user registration.
 // It corresponds to a basic user schema for registration in the database and
 // session cache.
-type Register struct {
+type User struct {
 	Username   string
 	Password   string
 	Email      string
 	Persistent string
-	save       func(interface{}) error
+	save       func(interface{}, execution.Context, http.ResponseWriter, *http.Request) error
 }
 
-func (rr Register) ServeHTTP(ctx execution.Context, w http.ResponseWriter, r *http.Request) {
+// New creates a user data hoding object with a user registration hook.
+func New(save func(interface{}, execution.Context, http.ResponseWriter, *http.Request) error) User {
+	u := User{}
+	u.save = save
+	return u
+}
+
+// Save registers a user, can write whether the operation succeeded to w.
+func (u User) Save(ctx execution.Context, w http.ResponseWriter, r *http.Request) {
+	if u.save != nil {
+		err := u.save(u, ctx, w, r)
+		if err != nil {
+			panic(errors.New(err.Error()))
+		}
+	}
+}
+
+func (u User) ServeHTTP(ctx execution.Context, w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
 		panic(errors.New(err.Error()))
 	}
 
-	user := Register{
-		Username:   r.Form.Get("username"),
-		Password:   r.Form.Get("password"),
-		Email:      r.Form.Get("email"),
-		Persistent: r.Form.Get("persistent"),
-	}
+	u.Username = r.Form.Get("username")
+	u.Password = r.Form.Get("password")
+	u.Email = r.Form.Get("email")
+	u.Persistent = r.Form.Get("persistent")
 
 	// Then we save this user in the database and what not.
 	// The funcion is in charge of the sanitization of the data.
-	if rr.save != nil {
-		err := rr.save(user)
-		if err != nil {
-			panic(errors.New(err.Error()))
-		}
-	}
-
-	fmt.Fprint(w, "Registration successful. An email was sent for confirmation.")
+	// That function may panic but that will be caught up by a panic handler.
+	u.Save(ctx, w, r)
 }
